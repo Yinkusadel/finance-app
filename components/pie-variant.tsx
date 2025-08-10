67db9192574dc9b719e77f00692747dff223fe43
olayinka <adeleyeolayinka73@gmail.com>
2025-08-10 22:51:46 +0100

    add complete charts

---
+++ components/pie-variant.tsx
@@ -0,0 +1,84 @@
+import {
+  Tooltip,
+  ResponsiveContainer,
+  Cell,
+  PieChart,
+  Pie,
+  Legend,
+  LegendPayload,
+} from "recharts";
+import { formatPercentage } from "@/lib/utils";
+import CategoryTooltip from "@/components/category-tooltip";
+
+const COLORS = ["#0062FF", "#12C6FF", "#FF647F", "#FF9354"];
+
+type Props = {
+  data?: {
+    value: number;
+    name: string;
+  }[];
+};
+
+const PieVariant = ({ data }: Props) => {
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <PieChart>
+        <Legend
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="right"
+          iconType="circle"
+          content={(props) => {
+            const payload = props.payload as LegendPayload[] | undefined;
+            return (
+              <ul className="flex flex-col space-y-2">
+                {payload?.map((entry, index) => (
+                  <li
+                    key={`item-${index}`}
+                    className="flex items-center space-x-2"
+                  >
+                    <span
+                      className="size-2 rounded-full"
+                      style={{
+                        backgroundColor: entry.color,
+                      }}
+                    />
+                    <div className="space-x-1">
+                      <span className="text-sm text-muted-foreground">
+                        {entry.value}
+                      </span>
+                      <span className="text-sm">
+                        {formatPercentage(
+                          ((entry.payload as { percent: number }).percent ?? 0) *
+                            100
+                        )}
+                      </span>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            );
+          }}
+        />
+        <Tooltip content={CategoryTooltip} />
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          outerRadius={90}
+          innerRadius={60}
+          paddingAngle={2}
+          fill="#8884d8"
+          dataKey="value"
+          labelLine={false}
+        >
+          {data?.map((_entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default PieVariant;
