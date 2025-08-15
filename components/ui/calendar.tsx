61cc186e353c61ebba949081f0dcbc6cc2418fb3
olayinka <adeleyeolayinka73@gmail.com>
2025-08-15 11:03:41 +0100

    update calender in components due to update

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -60,13 +60,14 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
+        Chevron: ({ orientation, ...props }) =>
+          orientation === "left" ? (
+            <ChevronLeft className="size-4" {...props} />
+          ) : (
+            <ChevronRight className="size-4" {...props} />
+          ),
       }}
+
       {...props}
     />
   )
