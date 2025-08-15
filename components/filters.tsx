f862f1350bf43918d442a6d8e595c67d318db419
olayinka <adeleyeolayinka73@gmail.com>
2025-08-15 11:47:37 +0100

    make accounts page use suspense

--- components/filters.tsx
+++ components/filters.tsx
@@ -5,7 +5,9 @@ import DateFilter from "@/components/date-filter";
 const Filters = () => {
   return (
     <div className="flex flex-col lg:flex-row items-center gap-y-2 lg:gap-y-0 lg:gap-x-2">
-      <AccountFilter />
+      <Suspense fallback={<div>Loading account filter...</div>}>
+        <AccountFilter />
+      </Suspense>
       <Suspense fallback={<div>Loading date filter...</div>}>
         <DateFilter />
       </Suspense>
