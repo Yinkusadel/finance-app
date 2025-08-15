650e165adedf0d16adf076a4323a9ef95902ebcf
olayinka <adeleyeolayinka73@gmail.com>
2025-08-15 11:52:55 +0100

    make accounts page use suspense

--- app/(dahsboard)/page.tsx
+++ app/(dahsboard)/page.tsx
@@ -1,11 +1,16 @@
+import { Suspense } from "react";
 import DataCharts from "@/components/data-charts";
 import DataGrid from "@/components/data-grid";
 
 export default function DashboardPage() {
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
-      <DataGrid />
-      <DataCharts />
+      <Suspense fallback={<div>Loading data grid...</div>}>
+        <DataGrid />
+      </Suspense>
+      <Suspense fallback={<div>Loading charts...</div>}>
+        <DataCharts />
+      </Suspense>
     </div>
   );
 }
